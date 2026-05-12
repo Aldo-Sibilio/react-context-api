5ec78c8098171fa000276c0d3b8253309e42a91e
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-05-12 16:12:43 +0200

    aggiunta configurazione React Router con le 3 rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ChiSiamoPage from "./pages/ChiSiamoPage";
+import ProductsPage from "./pages/ProductsPage";
+
 function App() {
   return (
-    <div>App</div>
-  )
+    // BrowserRouter abilita il routing dell'app
+    <BrowserRouter>
+      <Navbar />
+      {/* Routes contiene tutte le rotte dell'app */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chi-siamo" element={<ChiSiamoPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
-export default App
+
+export default App;
